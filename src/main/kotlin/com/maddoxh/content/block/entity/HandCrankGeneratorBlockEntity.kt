b29fef9eeb3e7329c1d79f5f299d4d5737327ed0
Aznos <91508926+Aznos@users.noreply.github.com>
2025-07-07 18:53:35 -0400

    Add a 2-second delay for cranking

--- src/main/kotlin/com/maddoxh/content/block/entity/HandCrankGeneratorBlockEntity.kt
+++ src/main/kotlin/com/maddoxh/content/block/entity/HandCrankGeneratorBlockEntity.kt
@@ -11,10 +11,15 @@ import net.minecraft.util.math.BlockPos
 class HandCrankGeneratorBlockEntity(pos: BlockPos, state: BlockState) : BlockEntity(ModBlockEntities.HAND_CRANK_GENERATOR, pos, state), EnergyStorage {
     private var storedEu: Long = 0
     private val capacity: Long = 250
+    private var lastCrankTime: Long = 0
 
     fun crank(): Boolean {
+        val now = System.currentTimeMillis()
+        if(now - lastCrankTime < 2000) return false
+
         if(storedEu + 25 <= capacity) {
             storedEu += 25
+            lastCrankTime = now
             markDirty()
             return true
         }
