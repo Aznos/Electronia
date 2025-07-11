43b560d73ad845799c845fc5011de13c9f616d73
Aznos <91508926+Aznos@users.noreply.github.com>
2025-07-11 14:42:29 -0400

    Added a recipe for the crank press

--- src/main/kotlin/com/maddoxh/datagen/ModRecipeGenerator.kt
+++ src/main/kotlin/com/maddoxh/datagen/ModRecipeGenerator.kt
@@ -1,5 +1,6 @@
 package com.maddoxh.datagen
 
+import com.maddoxh.registry.ModBlocks
 import com.maddoxh.registry.ModItems
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
@@ -20,6 +21,22 @@ class ModRecipeGenerator(output: FabricDataOutput, registriesFuture: Completable
             .input('S', Blocks.STONE)
             .criterion("has_oak_planks", conditionsFromItem(Blocks.OAK_PLANKS))
             .criterion("has_stone", conditionsFromItem(Blocks.STONE))
+            .offerTo(exporter)
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.CRANK_PRESS)
+            .pattern(" #S")
+            .pattern(" @P")
+            .pattern("CPS")
+            .input('#', Blocks.PISTON)
+            .input('@', Blocks.STONE_PRESSURE_PLATE)
+            .input('S', Blocks.OAK_STAIRS)
+            .input('P', Blocks.OAK_PLANKS)
+            .input('C', ModItems.CRANK)
+            .criterion("has_piston", conditionsFromItem(Blocks.PISTON))
+            .criterion("has_stone_pressure_plate", conditionsFromItem(Blocks.STONE_PRESSURE_PLATE))
+            .criterion("has_oak_stairs", conditionsFromItem(Blocks.OAK_STAIRS))
+            .criterion("has_oak_planks", conditionsFromItem(Blocks.OAK_PLANKS))
+            .criterion("has_crank", conditionsFromItem(ModItems.CRANK))
             .offerTo(exporter)
     }
 }
